4cb64a360d1141fbe0edb179898debdec0098145
Mr. Eric James Jameson <ejc84332@ac-websrv-h2.its.bethel.edu>
2017-04-20 22:45:58 +0000

    add plugins code

--- theme/clean/version.php
+++ theme/clean/version.php
@@ -31,8 +31,8 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2016120500;
-$plugin->requires  = 2016112900;
+$plugin->requires  = 2016120500;
 $plugin->component = 'theme_clean';
 $plugin->dependencies = array(
-    'theme_bootstrapbase'  => 2016112900,
+    'theme_bootstrapbase'  => 2016120500,
 );
