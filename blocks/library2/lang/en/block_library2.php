f218be946a726fdc7084a0574e2cd33cce81965f
apache <webmaster@bethel.edu>
2018-03-13 14:52:55 -0500

    Updated block_library with new content from block_library2 and deleted block_library2

--- blocks/library2/lang/en/block_library2.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$string['pluginname'] = 'BU Library2 Block';
-	$string['library2'] = 'Library2';
-	$string['library2:addinstance'] = 'Add a new Library2 block';
-	$string['library2:myaddinstance'] = 'Add a new Library2 block to the My Moodle page';
-?>
